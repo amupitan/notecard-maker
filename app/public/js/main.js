7aba282f229fda15eabaaf8bf40091b9eb39988c
= <mich.menning@gmail.com>
2017-03-25 20:55:25 -0500

    Added Next and show solution functionalities

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,14 +1,56 @@
-
 let note = document.getElementById('notecards');
+
+//These questions are only until we get the actual one passed in
+var notes = {
+ "Subject" : "Random",
+ "Questions":{
+   "1+1":"2",
+   "Gadia's password":"Tuples",
+   "Value of PI":"3.1415926...",
+   "Best NBA player":"Georges Niange",
+   "Capital of Iowa":"Ames",
+   "Least liked team in NBA":"Golden State",
+   "Are all CA's lame?": "Yes",
+   "Shaeffer's baby is what?": "Program written in cpp",
+   "Easiest class at ISU":"COMS327"
+ }
+}
+
+document.getElementById("sub").innerHTML = notes.Subject;
+
+var questions = notes.Questions;
+var quest;
+var answ;
+var numQuestion =0;
+var rand_num; //Question number
+for(let i in questions){numQuestion++}; //Get number of questions there are
 
 let next = document.getElementById('nextQ');
 next.onclick=function(){
-  // let q = document.createElement('article');
-  // q.setAttribute('id','question');
+  //TO prevent same question back to back
+  let a;
+  do{
+     a = Math.floor((Math.random() * numQuestion));
+  } while (rand_num == a);
+  rand_num = a;
+
+  var iter = -1;
+  for(let i in questions){
+    quest = i;
+    answ = questions[i];
+    iter ++;
+    if(iter == rand_num) break;
+  }
   let q = document.getElementById('question');
+  q.className = "que"
   q.innerHTML ="";
-  q.innerHTML= "2+3";
-  note.appendChild(q);
-  console.log("Next");
+  q.innerHTML= quest;
 }
-// note.style.color = "red";
+
+let solution = document.getElementById('seeAnswer');
+solution.onclick=function(){
+  let q = document.getElementById('question');
+  q.className = "sol";
+  q.innerHTML ="";
+  q.innerHTML= answ;
+}
--- app/routes/notes.js
+++ app/routes/notes.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router =express.Router();
 
 router.get('/notes', function(request, response){
-	var dataFile = request.app.get('appData');
-
-  console.log("data is " +dataFile.Notes.Subject);
-  console.log("Questions are: " + dataFile.Notes.Questions);
-
-  let quest = dataFile.Notes.Questions;
-  var questions ="";
-  console.log(quest);
-  for(let i in quest){
-    // console.log("Question: " + quest[i]);
-    questions += `
-      <p>${i}</p>
-      <p>${quest[i]}</p>
-    `
-    console.log(i + " : " + quest[i]);
-  }
+	// var dataFile = request.app.get('appData');
+	//
+  // console.log("data is " +dataFile.Notes.Subject);
+  // console.log("Questions are: " + dataFile.Notes.Questions);
+	//
+  // let quest = dataFile.Notes.Questions;
+  // var questions ="";
+  // console.log(quest);
+  // for(let i in quest){
+  //   // console.log("Question: " + quest[i]);
+  //   questions += `
+  //     <p>${i}</p>
+  //     <p>${quest[i]}</p>
+  //   `
+  //   console.log(i + " : " + quest[i]);
+  // }
   // response.send(
   //   `
   //   ${questions}
