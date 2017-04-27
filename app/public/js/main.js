56bd64f7497a1ea3dd91b89b29c9a3d386451d51
= <mich.menning@gmail.com>
2017-04-26 20:47:57 -0500

    In notes_select, clicking on div checks the checkbox

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -26,12 +26,20 @@ var notes;
             document.getElementById('question').className = "que";
             document.getElementById('question').innerHTML = notes[rand_num][0];
             document.getElementById('side').innerHTML = "Title";
-            // console.log("NEXT");
           };
           document.getElementById('seeAnswer').onclick = () => {
-            document.getElementById('question').className = (notes[rand_num][1] === document.getElementById('question').innerHTML ? "que" : "sol");
-            // if(notes[rand_num][1] ===  document.getElementById('question').innerHTML)
-            let card_flip =(notes[rand_num][1] ===  document.getElementById('question').innerHTML ? notes[rand_num][0] : notes[rand_num][1]);
+            let card_flip;
+            if(notes[rand_num][1] === document.getElementById('question').innerHTML){
+              document.getElementById('question').className = "que";
+              card_flip = notes[rand_num][0];
+              document.getElementById('side').innerHTML="Title";
+            }
+            else{
+              document.getElementById('question').className = "sol";
+              card_flip=notes[rand_num][1];
+              document.getElementById('side').innerHTML="Description";
+            }
+
             if (typeof card_flip === "object"){
               let temp = card_flip;
               card_flip = document.createElement('ol');
@@ -44,9 +52,6 @@ var notes;
             }else{
               document.getElementById('question').innerHTML = card_flip;
             }
-            // document.getElementById('question').appendChild(card_flip);//notes[rand_num][1];
-            // document.getElementById('question').innerHTML = notes[rand_num][1];
-            document.getElementById('side').innerHTML =(notes[rand_num][1] != document.getElementById('question').innerHTML ? "Title" : "Description");
           };
         }
   });
